237ebcc649a1fcfa9fd7d09c90d4439e979407b6
Jack Pantalena <jackpantalena@Jacks-MacBook-Air.local>
2017-04-08 15:47:44 -0600

    added migration and seed routes to knexfile

--- knexfile.js
+++ knexfile.js
@@ -13,6 +13,12 @@ module.exports = {
   },
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: __dirname + '/src/server/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/src/server/db/seeds'
+    }
   }
 };
